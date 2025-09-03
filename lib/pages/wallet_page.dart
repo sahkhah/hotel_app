f00802f58db1f14fba9fc2d2bd54e6837829059b
Kamil's Dev <sakakamil18@gmail.com>
2025-09-03 10:03:57 -0700

    started working on the wallet page

--- lib/pages/wallet_page.dart
+++ lib/pages/wallet_page.dart
@@ -1,3 +1,4 @@
+import 'package:book_hotel/services/widget_support.dart';
 import 'package:flutter/material.dart';
 
 class WalletPage extends StatefulWidget {
@@ -10,6 +11,23 @@ class WalletPage extends StatefulWidget {
 class _WalletPageState extends State<WalletPage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+       appBar: AppBar(
+        centerTitle: true,
+        title: Text('Bookings', style: AppWidget.headLineTextStyle(30.0)),
+      ),
+      body: Container(
+        child: Column(
+          children: [
+            Container(
+               decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(20.0),
+                          color: Color(0xffececf8),
+                        ),
+            )
+          ],
+        )
+      ),
+    );
   }
 }
